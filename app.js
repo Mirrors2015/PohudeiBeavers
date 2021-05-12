0e01efa486c79a5a75831cddb5ca21dad810ac97
denis <denis.botvinovsky@gmail.com>
2021-05-12 23:05:48 +0300

    working + favicon v 0.0.2

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const hbs = require('hbs')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const hbs = require("hbs");
 
-const indexRouter = require('./routes/index');
-const registrationRouter = require('./routes/registration');
-const adminRouter = require('./routes/admin');
-const constructorRouter = require('./routes/constructor');
-const loginRouter = require('./routes/login');
-const profileRouter = require('./routes/profile');
+const indexRouter = require("./routes/index");
+const registrationRouter = require("./routes/registration");
+const adminRouter = require("./routes/admin");
+const constructorRouter = require("./routes/constructor");
+const loginRouter = require("./routes/login");
+const profileRouter = require("./routes/profile");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/registration', registrationRouter);
-app.use('/admin', adminRouter);
-app.use('/constructor', constructorRouter);
-app.use('/login', loginRouter);
-app.use('/profile', profileRouter);
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
+
+app.use("/", indexRouter);
+app.use("/registration", registrationRouter);
+app.use("/admin", adminRouter);
+app.use("/constructor", constructorRouter);
+app.use("/login", loginRouter);
+app.use("/profile", profileRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
