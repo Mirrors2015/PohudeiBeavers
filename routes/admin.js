1ae5f0cc5ccd045c1d59d384137317ac5851ccf9
denis <denis.botvinovsky@gmail.com>
2021-05-13 11:17:32 +0300

    reg work v 0.0.3

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 router.get("/", function (req, res, next) {
   res.render("partials/admin");
 });
--- routes/login.js
+++ routes/login.js
@@ -4,22 +4,21 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.render("partials/login");
 });
-router.post("/constructor", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-      res.sendStatus(400);
-    }
-    const validPassword = bcrypt.compareSync(password, user.password);
-    if (!validPassword) {
-      res.sendStatus(401);
-    }
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(418);
-    osfdkfo;
-  }
-});
+// router.post("/constructor", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       res.sendStatus(400);
+//     }
+//     const validPassword = bcrypt.compareSync(password, user.password);
+//     if (!validPassword) {
+//       res.sendStatus(401);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.sendStatus(418);
+//   }
+// });
 
 module.exports = router;
