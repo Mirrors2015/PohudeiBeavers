62ba91364b634916106b818d432a7ab5c77fcd99
denis <denis.botvinovsky@gmail.com>
2021-05-13 10:29:53 +0300

    working reg v 0.0.3

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("partials/login");
+});
+router.post("/constructor", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.sendStatus(400);
+    }
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(418);
+    osfdkfo;
+  }
 });
 
 module.exports = router;
--- routes/registration.js
+++ routes/registration.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { User } = require("../models/User");
+const { Role } = require("../models/Role");
+const bcrypt = require("bcrypt");
 
-router.get("/", function (req, res, next) {
-  res.render("partials/registration");
-});
+router
+  .route("/registration")
+  .get("/", (req, res, next) => {
+    res.render("partials/registration");
+  })
+  .post("/constructor", async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res
+          .sendStatus(400)
+          .json({ message: "Пользователь с таким эл.адресом уже существует" });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: "User" });
+      const user = new User({
+        username,
+        email,
+        password: hashPassword,
+        roles: [userRole.value],
+      });
+      await user.save();
+      return res.json({ message: "Пользователь успешно зарегистрован" });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(418);
+    }
+  });
 
 module.exports = router;
